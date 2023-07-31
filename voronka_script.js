be3f0cbb92569c6c0821123ae474c90ff12d7253
Sand Despotism <wowow3722123@gmail.com>
2023-07-31 16:15:12 +0300

    for god and glory

--- voronka_script.js
+++ voronka_script.js
@@ -1,3 +1,4 @@
+importScripts('https://checkout.cloudpayments.ru/checkout.js');
 let plan_boxes = document.getElementsByClassName('plan_box');
 let round_divs = document.getElementsByClassName('plan_box_time_round');
 console.log(round_divs);
@@ -11,4 +12,20 @@ function change_plan_box(plan_box){
     plan_boxes[plan_box].style.borderColor = color;
     round_divs[plan_box].style.backgroundColor = color;
 }
+const checkout = new cp.Checkout({
+	publicId: 'pk_8cef82aea94ac4411ffec9597022d',
+});
+const fieldValues = {
+    cvv: '911',
+    cardNumber: '4242 4242 4242 4242',
+    expDateMonth: '12',
+    expDateYear: '24',
+}
+
+checkout.createPaymentCryptogram(fieldValues)
+    .then((cryptogram) => {
+        console.log(cryptogram);
+    }).catch((errors) => {
+        console.log(errors)
+});
 
